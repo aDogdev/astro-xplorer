6ea51fb4a1f600652a9700f00cdb24a622726bb3
adog <adrianmg3008@gmail.com>
2024-10-23 12:11:31 -0400

    change css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
 import "./Header.css";
 
 function Header() {
-  return <h1>SpaceXplorer Test</h1>;
+  return (
+    <header>
+      <h1 className="logo">
+        Space<span>X</span>plorer
+      </h1>
+      <button>Theme</button>
+      <form className="search-form">
+        <input type="search" placeholder="Starlink, Falcon, Galaxy..."></input>
+        <select name="" id="">
+          <option></option>
+        </select>
+        <input type="date" />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+  );
 }
 
 export { Header };
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -2,11 +2,7 @@ import "./Tag.css";
 
 function Tag({ isSuccess }) {
   const missionState = isSuccess ? "success" : "failure";
-  return (
-    <span className={missionState}>
-      {missionState.toUpperCase()}
-    </span>
-  );
+  return <span className={missionState}>{missionState.toUpperCase()}</span>;
 }
 
 export { Tag };
